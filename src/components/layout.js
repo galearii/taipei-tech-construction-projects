7ca50c44d789e0c8751435516c28f450e1f2089d
John Chen <galearii@gmail.com>
2020-04-06 14:28:36 +0800

    Styled project template and main layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,10 +4,10 @@ import Footer from "./footer";
 
 function Layout({ children }) {
   return (
-    <div className="flex flex-col font-sans min-h-screen text-gray-900">
+    <div className="flex flex-col font-sans min-h-screen text-gray-900 bg-gray-100">
       <Header />
 
-      <main className="flex flex-col flex-1 max-w-4xl mx-auto px-4 py-8 md:p-8 w-full">
+      <main className="flex flex-col flex-1 max-w-6xl mx-auto px-4 py-8 md:p-8 w-full bg-white">
         {children}
       </main>
 
--- src/templates/project-detail.js
+++ src/templates/project-detail.js
@@ -8,7 +8,11 @@ export default ({ data }) => {
   return (
     <Layout>
       <div>
-        <h1>{post.frontmatter.title}</h1>
+        <h2 className="text-2xl font-bold mb-6">
+          <span className="border-solid border-b-4 border-blue-400 pb-2">
+            {post.frontmatter.title}
+          </span>
+        </h2>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
     </Layout>
@@ -18,6 +22,9 @@ export default ({ data }) => {
 export const query = graphql`
   query($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
+      fields {
+        slug
+      }
       html
       frontmatter {
         title
