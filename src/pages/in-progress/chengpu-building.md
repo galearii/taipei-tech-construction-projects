56bbe061788f982b3cd76587b6cb84ac674c607d
John Chen <galearii@gmail.com>
2020-04-07 16:00:13 +0800

    Styled the top section of a project detail page

--- src/pages/in-progress/chengpu-building.md
+++ src/pages/in-progress/chengpu-building.md
@@ -6,19 +6,9 @@ cost: "2 億 8260 萬元"
 schedule: "預計於 2021 年 6 月完工"
 ---
 
-<div>
-  <ul>
-    <li>地上11層(1-3樓商業空間、4-6樓推廣教育空間、7-9樓創新育成空間、10-11樓多用途及會空間)</li>
-    <li>地下3層(停車空間)</li>
-  </ul>
-</div>
+###樓層說明
+1. 地上11層(1-3樓商業空間、4-6樓推廣教育空間、7-9樓創新育成空間、10-11樓多用途及會空間)
+2. 地下3層(停車空間)
 
-<div>
-  <ul>
-    <li><span>總樓地板面積</span><span>4,447 平方公尺</span></li>
-    <li><span>總工程經費</span><span>2億8260萬元</span></li>
-    <li><span>相關期程</span><span>預計於2021年6月完工</span></li>
-  </ul>
-</div>
-
-<iframe width="560" height="315" src="https://www.youtube.com/embed/4n0xNbfJLR8" frameborder="0" allowfullscreen></iframe>
+###位置
+![Hopper The Rabbit](https://source.unsplash.com/random/800x600)
--- src/templates/project-detail.js
+++ src/templates/project-detail.js
@@ -13,6 +13,49 @@ export default ({ data }) => {
             {post.frontmatter.title}
           </span>
         </h2>
+
+        <div className="mb-6 pb-6 border-b md:flex md:justify-between">
+          <div className="md:w-49p lg:w-3/4">
+            <img src={post.fields.slug + "hero.jpg"} />
+          </div>
+          <div className="md:w-49p lg:pl-4">
+            <ul className="mt-2 md:mt-0">
+              <li className="p-2 pl-0">
+                <span className="rounded mr-2 p-2 bg-teal-200 border-l-4 border-teal-400">
+                  總樓地板面積
+                </span>
+                {post.frontmatter.floorArea}
+              </li>
+              <li className="p-2 pl-0">
+                <span className="rounded mr-2 p-2 bg-indigo-200 border-l-4 border-indigo-400">
+                  總工程經費
+                </span>
+                {post.frontmatter.cost}
+              </li>
+              <li className="p-2 pl-0">
+                <span className="rounded mr-2 p-2 bg-purple-200 border-l-4 border-purple-400">
+                  相關期程
+                </span>
+                {post.frontmatter.schedule}
+              </li>
+            </ul>
+            <div>
+              <a
+                className="block rounded p-4 text-xl text-white bg-green-500 hover:bg-green-700 mt-4 text-center"
+                href="https://newgiving.ntut.edu.tw/"
+              >
+                前往臺北科技大學捐贈網站
+                <svg
+                  className="w-4 inline-block ml-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 100 125">
+                    <path d="M50 2.5C23.8 2.5 2.5 23.8 2.5 50S23.8 97.5 50 97.5 97.5 76.2 97.5 50 76.2 2.5 50 2.5zm16.4 50.7L48 71.5c-.9.9-2 1.3-3.2 1.3s-2.3-.4-3.2-1.3c-1.7-1.7-1.7-4.6 0-6.3L56.9 50 41.7 34.8c-1.7-1.7-1.7-4.6 0-6.3 1.7-1.7 4.6-1.7 6.3 0l18.4 18.3c.8.8 1.3 2 1.3 3.2s-.5 2.3-1.3 3.2z" fill="#fff"/>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
     </Layout>
@@ -28,6 +71,9 @@ export const query = graphql`
       html
       frontmatter {
         title
+        floorArea
+        cost
+        schedule
       }
     }
   }
