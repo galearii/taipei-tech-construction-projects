abb8b2f7da28bc1414c75b38dfe369feeef7eb89
John Chen <galearii@gmail.com>
2020-05-02 06:39:59 +0800

    Added manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,5 +34,20 @@ module.exports = {
         purgeOnly: [`src/css/style.css`]
       }
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `臺北科技大學校園整建工程`,
+        short_name: `北科校園整建工程`,
+        start_url: `/`,
+        background_color: `#2B6CB0`,
+        theme_color: `#2B6CB0`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
   ]
 }
